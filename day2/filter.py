a0bb057cdc37ce1ef11b65c2dfdfa627e82d31bc
jkirshner23@gmail.com <cmdb@BIOL-CMDB-34.local>
2015-08-25 11:24:47 -0400

    Filter Lines 10-15

--- day2/filter.py
+++ day2/filter.py
@@ -4,9 +4,25 @@ filename = "/Users/cmdb/qbb2015/stringtie/SRR072893/t_data.ctab"
 
 f = open( filename )
 
-for line in f:
+"""for line in f:
     fields = line.split()
     if "tRNA" in fields[9]:
         print line, 
     # comma gets rid of the extra line 
-    # a for loop for a file always reads line by line 
+    # a for loop for a file always reads line by line"""
+    
+
+    
+for line_count, data in enumerate(f):
+    if line_count <=10:
+        pass
+    elif line_count <= 15:
+        print data,
+    else:
+        break 
+        
+# enumerate adds +1 to each 
+ 
+        
+                    
+          
